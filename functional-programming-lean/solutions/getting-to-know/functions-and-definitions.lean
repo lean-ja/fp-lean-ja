07c096d8960edffeed834be6f4ff74f13a3261c2
Seasawher <tellagoodstory@icloud.com>
2024-01-22 19:21:27 +0900

    1.1 と 1.2 の解答を追加

---
+++ functional-programming-lean/solutions/getting-to-know/functions-and-definitions.lean
@@ -0,0 +1,24 @@
+/- # 1.2 演習問題 -/
+
+/-! ## 1
+関数 `joinStringsWith` を `String -> String -> String -> String` 型の関数であって，その第一引数を第二引数と第三引数の間に配置して新しい文字列を作成するようなものとして定義してください．`joinStringsWith ", " "one" "and another"` は `"one, and another"` に等しくなるはずです.-/
+
+def joinStringsWith (s1 : String) (s2 : String) (s3 : String) : String :=
+  s2 ++ s1 ++ s3
+
+example : joinStringsWith ", " "one" "and another" = "one, and another" := rfl
+
+/-! ## 2
+`joinStringsWith ": "` の型は何でしょうか？ Lean で答えを確認してください．-/
+
+#check (joinStringsWith ": " : String → String → String)
+
+/-! ## 3
+与えられた高さ，幅，奥行きを持つ直方体の体積を計算する関数 `volume` を `Nat → Nat → Nat → Nat` 型の関数として定義してください．
+-/
+
+def volume (h : Nat) (w : Nat) (d : Nat) : Nat := h * w * d
+
+example : volume 3 4 5 = 60 := rfl
+
+#check (volume : Nat → Nat → Nat → Nat)
