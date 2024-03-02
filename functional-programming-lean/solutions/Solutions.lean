5ba26f883f278ed0a7132f228fb541323f099c9b
Seasawher <tellagoodstory@icloud.com>
2024-03-02 16:13:24 +0900

    import が通るように lake init する

---
+++ functional-programming-lean/solutions/Solutions.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `Solutions` library.
+-- Import modules here that should be built as part of the library.
+import Solutions.TypeClasses.Pos
+import Solutions.TypeClasses.Polymorphism
---
+++ functional-programming-lean/solutions/lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package «solutions» where
+  -- add package configuration options here
+
+@[default_target]
+lean_lib «Solutions» where
+  -- add library configuration options here
