125b2672c4a6ea53164c8ba2b12893f138854733
Seasawher <tellagoodstory@icloud.com>
2024-06-22 22:25:14 +0900

    set `globs` in lakefile

--- functional-programming-lean/solutions/Solutions.lean
+++
@@ -1,4 +0,0 @@
--- This module serves as the root of the `Solutions` library.
--- Import modules here that should be built as part of the library.
-import Solutions.TypeClasses.Pos
-import Solutions.TypeClasses.Polymorphism
